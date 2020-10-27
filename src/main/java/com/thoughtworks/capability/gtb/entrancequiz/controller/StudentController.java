ffa78c191c6f19a52168280c4044225f1b497603
zkjxx1548 <438101845@qq.com>
2020-10-27 23:51:49 +0800

    feat: add api addStudent

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -2,15 +2,25 @@ package com.thoughtworks.capability.gtb.entrancequiz.controller;
 
 import com.thoughtworks.capability.gtb.entrancequiz.data.Data;
 import com.thoughtworks.capability.gtb.entrancequiz.domian.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StudentController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        return Data.getStudents();
+    @GetMapping("/get/students")
+    public ResponseEntity<List<Student>> getStudents() {
+        return ResponseEntity.ok(Data.getStudents());
+    }
+
+    @PostMapping("/add/student")
+    public String addStudent(@RequestBody String name) {
+        List<Student> students = Data.getStudents();
+        students.add(new Student(students.size() + 1, name));
+        Data.setStudents(students);
+        System.out.println(Data.getStudents());
+        return "success";
     }
 }
